1cfd1f83ed4712ad99674fdc369c86ab8deb091f
Lipotam <Lipotam@gmail.com>
2012-09-02 19:22:19 +0300

    removed on vs2012 and LocalDB.

--- SlideX/Controllers/HomeController.cs
+++ SlideX/Controllers/HomeController.cs
@@ -19,8 +19,6 @@ namespace SlideX.Controllers
         public ActionResult Index()
         {
             ViewBag.Message = "Welcome to ASP.NET MVC!";
-
-           
             return View();
         }
 
